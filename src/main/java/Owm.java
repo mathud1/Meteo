e06fc30e4796bde58dcd2f35c787157e3d8091e8
mathud1 <mateusz.hudzikowski@gmail.com>
2018-09-13 20:51:42 +0200

    testowa klasa Owm implementująca api owm OwmUpdateStrategy włączające api owm do stacji Weather Station rozbudowana w celu uwzględnienia danych z owm api

---
+++ src/main/java/Owm.java
@@ -0,0 +1,45 @@
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.param.Weather;
+
+import java.util.List;
+
+public class Owm {
+
+    public static void main(String[] args)
+            throws APIException {
+
+
+        OWM owm = new OWM("***REMOVED***");
+
+
+        CurrentWeather cwd = owm.currentWeatherByCityName("Zug");
+
+
+        System.out.println("City: " + cwd.getCityName());
+
+
+        System.out.println("Temperature: " + (Math.round(cwd.getMainData().getTemp() - 273.15)) + " \'C");
+
+        System.out.println("Pressure: " + (cwd.getMainData().getPressure() + " hpa"));
+
+        System.out.println("Wind speed: " + (cwd.getWindData().getSpeed() + " m/s"));
+
+        System.out.println("Humidity: " + (cwd.getMainData().getHumidity()) + " %");
+
+        System.out.println("Cloud cover: " + (cwd.getCloudData().getCloud()) + " %");
+
+       /* if (cwd.hasRainData())
+            System.out.println("Rain: " + (cwd.getRainData().getPrecipVol3h()));*/
+
+        List<Weather> overall = cwd.getWeatherList();
+
+        for (Weather object: overall)
+            System.out.println(object.getMoreInfo());
+
+
+
+
+    }
+}
---
+++ src/main/java/OwmUpdateStrategy.java
@@ -0,0 +1,48 @@
+import net.aksingh.owmjapis.api.APIException;
+import net.aksingh.owmjapis.core.OWM;
+import net.aksingh.owmjapis.model.CurrentWeather;
+import net.aksingh.owmjapis.model.param.Weather;
+
+import java.util.List;
+
+/**
+ * Created by Matt on 11.09.2018 at 20:41.
+ */
+public class OwmUpdateStrategy implements UpdateStrategy {
+
+   private OWM owm;
+
+
+    public OwmUpdateStrategy() {
+        this.owm = new OWM("***REMOVED***");
+    }
+
+
+    public void update() {
+
+        CurrentWeather cwd = null;
+        try {
+            cwd = owm.currentWeatherByCityName("Zug");
+        } catch (APIException e) {
+            e.printStackTrace();
+        }
+
+        String localisation = cwd.getCityName();
+        double windSpeed = cwd.getWindData().getSpeed() ;
+        long temperature = Math.round(cwd.getMainData().getTemp() - 273.15);
+        double pressure = cwd.getMainData().getPressure();
+        double humidity = cwd.getMainData().getHumidity();
+        double cloudCover = cwd.getCloudData().getCloud();
+        List<Weather> overall = cwd.getWeatherList();
+
+        WeatherStation stacja = WeatherStation.INSTANCE;
+        stacja.setLocalisation(localisation);
+        stacja.setWind(windSpeed);
+        stacja.setTemperature(temperature);
+        stacja.setPressure(pressure);
+        stacja.setHumidity(humidity);
+        stacja.setCloudCover(cloudCover);
+        stacja.setOverall(overall);
+    }
+
+}
